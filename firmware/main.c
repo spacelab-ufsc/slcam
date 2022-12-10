14499f3f8b35fdad10a951b3ec4542326bf886a1
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2022-12-09 21:27:38 -0300

    firmware: Updating the version number

--- firmware/main.c
+++ firmware/main.c
@@ -26,7 +26,7 @@
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * \author Miguel Boing <miguelboing13@gmail.com>
  * 
- * \version 0.0.0
+ * \version 0.1.3
  * 
  * \date 2022/07/10
  * 
