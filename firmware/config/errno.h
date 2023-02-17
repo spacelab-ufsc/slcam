7b4613f2bbc2b0ed070a119775046ba8793067a3
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2023-02-17 08:21:00 -0300

    firmware: config: Adding metadata to the errno.h file #44

--- firmware/config/errno.h
+++ firmware/config/errno.h
@@ -1,3 +1,38 @@
+/*
+ * errno.h
+ * 
+ * Copyright The SLCam Contributors.
+ * 
+ * This file is part of SLCam.
+ * 
+ * SLCam is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SLCam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SLCam. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Errors definition.
+ * 
+ * \author Miguel Boing <miguelboing13@gmail.com>
+ * 
+ * \version 0.1.3
+ * 
+ * \date 2023/02/16
+ * 
+ * \defgroup errno Error Codes
+ * \{
+ */
+
 #ifndef ERRNO_H
 #define ERRNO_H
 
@@ -14,3 +49,5 @@
 #define ERRNO_DEVICE_FAILED_COM              0x22U       /** Failed to communicate */
 
 #endif /*ERRNO_H */
+
+/** \} End of errno group */
