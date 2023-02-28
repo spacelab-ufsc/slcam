73330caf045876f16d1ef1ef03393270170383c9
miguelboing <miguel-boing@hotmail.com>
2023-02-28 15:56:47 -0300

    firmware: config: Fixing formatting #44

--- firmware/config/errno.h
+++ firmware/config/errno.h
@@ -40,8 +40,8 @@
 #define ERRNO_SUCCESS                        0x00U       /** No errors found */
 
 /* Driver Level errors */
-#define ERRNO_DRIVER_NO_PORT                 0x11U       /**  No port found */
-#define ERRNO_DRIVER_NO_PARAMETER            0x12U       /**  Invalid parameter value */
+#define ERRNO_DRIVER_NO_PORT                 0x11U       /** No port found */
+#define ERRNO_DRIVER_NO_PARAMETER            0x12U       /** Invalid parameter value */
 #define ERRNO_DRIVER_FAILED                  0x13U       /** Failed to execute HAL function */             
 
 /* Device Level errors */
