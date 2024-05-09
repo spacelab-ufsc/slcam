625515a076310288786c6135d091b45bb1084beb
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2024-05-09 20:41:06 -0300

    firmware: Updating the main README file with the project license notice

--- firmware/README.md
+++ firmware/README.md
@@ -1,5 +1,5 @@
 <h1 align="center">
-    FIRMWARE PROJECT
+    SLCAM FIRMWARE PROJECT
     <br>
 </h1>
 
@@ -24,13 +24,17 @@
     <a href="https://github.com/spacelab-ufsc/slcam/graphs/contributors">
         <img alt="GitHub contributors" src="https://img.shields.io/github/contributors/spacelab-ufsc/slcam?color=yellow&style=for-the-badge">
     </a>
+    <a href="#license">
+        <img src="https://img.shields.io/badge/license-GPL3-yellow?style=for-the-badge">
+    </a>
 </p>
 
 <p align="center">
     <a href="#overview">Overview</a> •
     <a href="#dependencies">Dependencies</a> •
     <a href="#compiling">Compiling and building</a> •
-    <a href="#flashing">Flashing</a>
+    <a href="#flashing">Flashing</a> •
+    <a href="#license">License</a>
 </p>
 
 ## Overview
@@ -65,3 +69,7 @@ sudo dnf install gcc-arm-linux-gnu stlink
 ## Flashing
 
 ```make flash```
+
+## License
+
+This repository containing firmware files is licensed under GPLv3 license.
--- firmware/drivers/i2c/i2c.c
+++ firmware/drivers/i2c/i2c.c
@@ -1,22 +1,22 @@
 /*
  * i2c.c
  * 
- * Copyright The OBDH 2.0 Contributors.
+ * Copyright The SLCam Contributors.
  * 
- * This file is part of OBDH 2.0.
+ * This file is part of SLCam.
  * 
- * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * SLCam is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * OBDH 2.0 is distributed in the hope that it will be useful,
+ * SLCam is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with OBDH 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
+ * along with SLCam. If not, see <http:/\/www.gnu.org/licenses/>.
  * 
  */
 
